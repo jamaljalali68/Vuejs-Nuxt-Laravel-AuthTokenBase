6a7245166f3912e1eaaf900887b6d941746a9bc9
jamaljalali68 <seyedjamaljalali@gmail.com>
2023-01-24 12:28:45 +0330

    nuxt-auth-tokenBase created

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <NuxtLoadingIndicator />
+
+    <Header />
+    <NuxtPage />
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    runtimeConfig: {
+        public: {
+            apiBase: 'http://localhost:8000'
+        }
+    },
+    css: ['~/assets/css/main.css'],
+    
+    build: {
+        transpile: ['vue-toastification'],
+    },
+})
